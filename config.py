8cc4aed1de65e205942c2d01ba6c865aec613360
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-02-16 15:45:04 -0500

    Actualizando la URL a la base de datos

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:BG6Adg4eDF-b4ADFC343H4B12GHgbCaG@monorail.proxy.rlwy.net:57991/votacionescgao"
-    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://avnadmin:AVNS_RCmc2tNNNQMxA4YuGFv@mysql-29968a39-votacionescgao.a.aivencloud.com:24557/votacionescgao"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymsql://root:a5ba4g6C1bBG2Gec6dBBcHg6bCgbDF44@monorail.proxy.rlwy.net:16262/votacionescgao"
+    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://sql10684452:vC9lb8kQxG@sql10.freemysqlhosting.net:3306/votacionescgao"
     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@127.0.0.1:3306/votacionescgao"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
